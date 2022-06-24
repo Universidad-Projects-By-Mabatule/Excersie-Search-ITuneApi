8313e651ca44ecaf857b5a70b70506969c152918
mabatule1999 <maba1998a@gmail.com>
2022-06-24 14:57:26 -0400

    Add input search

--- exerciseSearchITune/src/app/app.module.ts
+++ exerciseSearchITune/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { ITuneSearchComponent } from './itune-search/itune-search.component';
 import {HttpClientModule} from '@angular/common/http'
 import {HttpClientJsonpModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import {ReactiveFormsModule} from '@angular/forms';
+
+//Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +21,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     BrowserModule,
     HttpClientModule,
     HttpClientJsonpModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- exerciseSearchITune/src/app/itune-search/itune-search.component.ts
+++ exerciseSearchITune/src/app/itune-search/itune-search.component.ts
@@ -3,7 +3,9 @@ import { ITuneApiService } from '../services/itune-api.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { IModelResultValues } from '../interfaces/imodel-result-values';
-import { IModelResultObjet } from '../interfaces/imodel-result-objet';
+
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-itune-search',
   templateUrl: './itune-search.component.html',
@@ -14,18 +16,26 @@ export class ITuneSearchComponent implements OnInit {
 
   public results: Array<IModelResultValues> = [];
   private unsubscribe: Subject<any> = new Subject<any>();
+  public formITune!: FormGroup;
 
-  constructor(private iTuneService:ITuneApiService,private cdRef: ChangeDetectorRef) { }
+  constructor(private iTuneService:ITuneApiService,private cdRef: ChangeDetectorRef,private formBuilder:FormBuilder) { }
 
-  ngOnInit(): void {
-    this.iTunesSerch();
+  public ngOnInit(): void {
+    this.formITune = this.formBuilder.group( {
+      searchInput:['',[Validators.required,Validators.required]],
+      selectMediaType: ['']
+    });
+
+    //this.iTunesSerch();
   }
   public iTunesSerch(){
-    this.iTuneService.getResultsITunes("Michael","musicArtist").pipe(
+    this.iTuneService.getResultsITunes(this.formITune.value.searchInput,"musicArtist").pipe(
           takeUntil(this.unsubscribe)
         ).subscribe(res => {
             this.results = res.results;
-            console.log(this.results)
+            console.log(this.results);
+
+
             this.cdRef.detectChanges();
           });
   }
